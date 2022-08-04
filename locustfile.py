006271c719394661662e7aac3d54cf9c11b60cff
oserifogullari18 <oserifogullari18@ku.edu.tr>
2022-08-04 17:11:32 +0300

    senaryo1

--- locustfile.py
+++ locustfile.py
@@ -1,26 +1,37 @@
 
+from urllib import response
 from locust import task, HttpUser, constant
 import logging
 import time
+import random
 
 class User(HttpUser):
     # Rate limiter have 10 token in 1 minute
     # A get request consumes 3 tokens, a post request consumes 5 tokens
-    # 1 get and 1 post will be requested, HTTP 200 will return, 8 tokens will be consumed
-    # The next get and post request will be responded with HTTP 429, it is marked as success
+    # 1 get and 1 post will be requested by the user with a random ip, HTTP 200 will return, 8 tokens will be consumed
+    # The next get and post request with the same ip will be responded with HTTP 429, it is marked as success
     # The process will be repeated after 60 seconds
+    # The code will be runned with a number of concurrent users
     wait_time = constant(60)
+    
+    num = random.randint(100,900)
+    # ip = determine(num)
+
 
     @task
     def first(self):
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.1'})
-        self.client.post('/post', headers={'x-fowarded-for': 'ip: 0.0.1'})
-        with self.client.get('/get', catch_response = True, headers={'x-fowarded-for': 'ip: 0.0.1'}) as resp:
+        # self.ip
+        ip = random.randint(100,900)
+        self.client.get('/get', headers={'x-fowarded-for': 'ip: %s' % str(ip)})
+        self.client.post('/post', headers={'x-fowarded-for': 'ip: %s' % str(ip)})
+        with self.client.get('/get', catch_response = True, headers={'x-fowarded-for': 'ip: %s' % str(ip)}) as resp:
             if resp.status_code == 429:
                 resp.success()
                 logging.info("Rate limiter is successful")
-        with self.client.post('/post', catch_response = True, headers={'x-fowarded-for': 'ip: 0.0.1'}) as resp:
+        with self.client.post('/post', catch_response = True, headers={'x-fowarded-for': 'ip: %s' % str(ip)}) as resp:
             if resp.status_code == 429:
                 resp.success()
                 logging.info("Rate limiter is successful")
+    
+ 
     
