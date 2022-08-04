167fa85cad723d7c71e71fa52042c67f661e1317
oserifogullari18 <oserifogullari18@ku.edu.tr>
2022-08-04 14:10:19 +0300

    429success

--- locustfile.py
+++ locustfile.py
@@ -1,35 +1,26 @@
 
 from locust import task, HttpUser, constant
 import logging
+import time
 
 class User(HttpUser):
     # Rate limiter have 10 token in 1 minute
-    # A get request consumes 3 tokens
-    # The application will respond 200 to 3 request and 429 to rest in 1 minute
-    # At the 8th 16th 24th 32nd 40th 48th 56th 64th 72nd 80th 88th 96th 104th 112nd 120th seconds locust will send a get request
-    # 9 of the 15 requests in 2 minutes will be banned
-    # Hence, approximately 60% of the requests will fail
-    wait_time = constant(8)
-
+    # A get request consumes 3 tokens, a post request consumes 5 tokens
+    # 1 get and 1 post will be requested, HTTP 200 will return, 8 tokens will be consumed
+    # The next get and post request will be responded with HTTP 429, it is marked as success
+    # The process will be repeated after 60 seconds
+    wait_time = constant(60)
 
     @task
-    def first_endpoint(self):
-
+    def first(self):
         self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.1'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.2'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.3'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.4'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.5'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.6'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.7'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.8'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.0.9'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.1.0'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.1.1'})
-        self.client.get('/get', headers={'x-fowarded-for': 'ip: 0.1.2'})
- 
-        
-
-
-
-
+        self.client.post('/post', headers={'x-fowarded-for': 'ip: 0.0.1'})
+        with self.client.get('/get', catch_response = True, headers={'x-fowarded-for': 'ip: 0.0.1'}) as resp:
+            if resp.status_code == 429:
+                resp.success()
+                logging.info("Rate limiter is successful")
+        with self.client.post('/post', catch_response = True, headers={'x-fowarded-for': 'ip: 0.0.1'}) as resp:
+            if resp.status_code == 429:
+                resp.success()
+                logging.info("Rate limiter is successful")
+    
